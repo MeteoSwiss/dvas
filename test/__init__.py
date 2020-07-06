1e1dbe5300fb36ea35c690683765c50f4de53c29
mol <luca.modolo@meteoswiss.ch>
2020-07-06 08:44:43 +0200

    Debug

--- test/__init__.py
+++ test/__init__.py
@@ -6,3 +6,9 @@ Distributed under the terms of the GNU General Public License v3.0 or later.
 SPDX-License-Identifier: GPL-3.0-or-later
 
 """
+
+from dvas import package_path
+import glob
+
+for p in package_path.rglob('*'):
+    print(p)
