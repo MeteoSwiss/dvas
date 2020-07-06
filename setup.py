e9e68526c65410a11a575fdcb9e8818e49c88bd0
mol <luca.modolo@meteoswiss.ch>
2020-07-06 15:38:50 +0200

    Move all into MANIFEST.in

--- setup.py
+++ setup.py
@@ -79,13 +79,14 @@ setup(
     ],
 
     include_package_data=True,  # So that non .py files make it onto pypi, and then back !
-    package_data={
-        'pytest_cfg': ['pytest.ini'],
-        'pylint_cfg': ['.pylintrc'],
-        'examples_files': ['./examples/*'],
-        'test_files': ['./test/*'],
-        'plot_style': ['*.mplstyle'],
-        #'docs': ['../docs/build']
-    },
+    # package_data={
+    #     "": ['*.csv', '*.txt', '*.yml', '*.yaml']
+    #     'pytest_cfg': ['pytest.ini'],
+    #     'pylint_cfg': ['.pylintrc'],
+    #     'examples_files': ['./examples/*'],
+    #     'test_files': ['./test/*'],
+    #     'plot_style': ['*.mplstyle'],
+    #     #'docs': ['../docs/build']
+    # },
 
 )
