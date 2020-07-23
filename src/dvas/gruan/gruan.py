72ab4f4aa1617025e8c2ee40a4b0c80d531e00b7
fpavogt <fpavogt@users.noreply.github.com>
2020-07-23 19:20:05 +0200

    Add merge_andor_rebin_gdps function

--- src/dvas/gruan/gruan.py
+++ src/dvas/gruan/gruan.py
@@ -14,11 +14,11 @@ This module contains GRUAN-related routines, including correlation rules for GDP
 import warnings
 import numpy as np
 
-def corcoef_gdp(i, j, uc_type,
-                sn_i=None, sn_j=None,
-                rig_i=None, rig_j=None,
-                event_i=None, event_j=None,
-                site_i=None, site_j=None):
+def corcoef_gdps(i, j, uc_type,
+                 sn_i=None, sn_j=None,
+                 rig_i=None, rig_j=None,
+                 event_i=None, event_j=None,
+                 site_i=None, site_j=None):
     ''' Computes the correlation coefficient(s), for the specific uncertainty types of GRUAN Data
     Products (GDPs), between specific measurements.
 
@@ -105,3 +105,255 @@ def corcoef_gdp(i, j, uc_type,
         raise Exception("Ouch ! valid uc_type are ['sigma_u', 'sigma_e', 'sigma_s', 'sigma_t']")
 
     return corcoef
+
+
+def merge_andor_rebin_gdps(profiles, sigma_us, sigma_es, sigma_ss, sigma_ts,
+                           sns=None, rigs=None, evts=None, sites=None,
+                           binning=1, method='weighted mean'):
+    ''' Combines and (possibly) rebin profiles with full error propagation.
+
+    Args:
+        profiles (list of ndarray): list of profiles to combine. All must have the same length!
+        sigma_us (list of ndarray): list of associated uncorrelated errors.
+        sigma_es (list of ndarray): list of associated environmental-correlated errors.
+        sigma_ss (list of ndarray): list of associated spatial-correlated errors.
+        sigma_ts (list of ndarray): list of associated temporal-correlated errors.
+        sns (list of numpy.ndarray of int or str, optional): list of RS Serial Numbers.
+        rigs (list of numpy.ndarray of int or str, optional): list of associated rigs.
+        events (list of numpy.ndarray of int or str, optional): list of associated events.
+        sites (list of numpy.ndarray of int or str, optional): list of associated sites.
+        binning (int, optional): the number of profile steps to put in a bin. Defaults to 1.
+        method (str, optional): combination rule. Can be 'weighted mean', or 'delta'.
+            Defaults to 'weighted mean'.
+
+    Returns:
+        (ndarray, ndarray, ndarray, ndarray, ndarray, ndarray, ndarray): the merged profile, 
+           merged sigma_u, merged sigma_e, merged_sigma_s, merged sigma_t, 
+           merged indices, new indices.
+
+    '''
+
+    # Some safety checks first of all
+    for (ind, item) in enumerate([profiles, sigma_us, sigma_es, sigma_ss, sigma_ts, 
+                                  sns, rigs, evts, sites]):
+        if ind > 4 and item is None:
+            # Only allow the optional components to be None.
+            continue
+        if not isinstance(item, list):
+            raise Exception('Ouch ! I was expecting a list (even with only 1 profile) !')
+        if len(item) != len(profiles):
+            raise Exception('Ouch ! I need the same number of profiles and errors for each types !')
+
+    if not isinstance(binning, int):
+        raise Exception('Ouch! binning must be of type int, not %s' % (type(binning)))
+    if binning <= 0:
+        raise Exception('Ouch! binning must be greater or equal to 1 !')
+
+    if method not in ['weighted mean', 'delta']:
+        raise Exception('Ouch ! Method %s unsupported.' % (method))
+
+    # How many gdps do we have ?
+    n_profiles = len(profiles)
+    # How long are the profiles ?
+    n_steps = len(profiles[0])
+
+    # For a delta , I can only have two profiles
+    if method == 'delta' and n_profiles != 2:
+        raise Exception('Ouch! I can only make the delta between 2 GDPs, not %i !' % (n_profiles))
+
+    # Check that all the profiles/errors have the same length
+    for item in [profiles, sigma_us, sigma_es, sigma_ss, sigma_ts]:
+        for p_ind in range(n_profiles):
+            if len(item[p_ind]) != n_steps:
+                raise Exception('Ouch ! All profiles and errors should have the same length!')
+
+    # let's get started
+    if method == 'weighted mean':
+
+        # Let's compute the total error for each profile
+        sigma_tots = [np.sqrt(sigma_us[p_ind]**2 +
+                              sigma_es[p_ind]**2 +
+                              sigma_ss[p_ind]**2 +
+                              sigma_ts[p_ind]**2) for p_ind in range(n_profiles)]
+
+        # Let's compute the weights vectors for each profile
+        w_ps = np.array([1/sigma_tot**2 for sigma_tot in sigma_tots])
+
+        # Let's assemble the arrays of w * x for all profiles
+        wx_ps = np.array([w_ps[p_ind] * profiles[p_ind] for p_ind in range(n_profiles)])
+
+        # Sum these along the number of probes
+        wx_s = np.nansum(wx_ps, axis=0)
+        w_s = np.nansum(w_ps, axis=0)
+
+        # Then sum these along the altitude layers according to the binning
+        # Make sure to deal with potential NaN's by using the where function to swap them with 0's
+        # This is because I can't find the reduceat function
+        wx_ms = np.add.reduceat(np.where(np.isnan(wx_s), 0, wx_s), range(0, n_steps, binning))
+        w_ms = np.add.reduceat(np.where(np.isnan(w_s), 0, w_s), range(0, n_steps, binning))
+
+        # Compute the weighted mean
+        # To avoid some runtime Warning, replace any 0 weight with nan's
+        x_ms = wx_ms / np.where(w_ms == 0, np.nan, w_ms)
+
+    elif method == 'delta':
+
+        # Compute the difference between the two profiles (full resolution)
+        delta_pqs = profiles[0] - profiles[1]
+
+        # Then sum these along the altitude layers according to the binning
+        # Make sure to deal with potential NaN's by using the where function to swap them with 0's
+        delta_pqm = np.add.reduceat(np.where(np.isnan(delta_pqs), 0, delta_pqs),
+                                    range(0, n_steps, binning))
+
+        # Build the mean by normalizing the sum by the number of altitude steps combined
+        x_ms = delta_pqm / np.add.reduceat(np.ones(n_steps), range(0, n_steps, binning))
+
+    # What is the length of my new profile ?
+    n_steps_new = len(x_ms)
+
+    # Which layer indexes from the original profiles are included in each step ?
+    # Watch out for the different syntax between np.split and np.add.reduceat when specifying the
+    # slices !
+    old_inds = np.split(range(n_steps), range(binning, n_steps, binning))
+
+    # I will also compute the pseudo index of the new binned levels
+    # Note: I take it as the simple mean of all the levels involved ... EVEN if this is a weighted
+    # sum ! These indices are therefore only meant for quick plotting purposes.
+    new_inds = np.array([np.mean(old_ind) for old_ind in old_inds])
+
+    # Run some sanity checks before I continue
+    if len(new_inds) != n_steps_new or len(old_inds) != n_steps_new:
+        raise Exception('Ouch ! This error should be impossible !')
+
+    # Let's get started with the computation of the errors. For now, let's do them one layer k at a
+    # time.
+    # Note: this is much (!) faster than implementing a full matrix approach, because these get
+    # very large but are mostly filled with 0's. However, this technique does not provide any
+    # information regarding the correlation of errors between different layer of the merged and
+    # resampled spectra. For now, this is not needed, and therefore this remains an appropriate
+    # choice. vof - 2020.04.16
+    # Wait ... is that statement actually correct ? vof - 2020.06.25
+
+    sigma_us_new = np.zeros_like(x_ms)
+    sigma_es_new = np.zeros_like(x_ms)
+    sigma_ss_new = np.zeros_like(x_ms)
+    sigma_ts_new = np.zeros_like(x_ms)
+
+    # For each layer k, we will arrange all the measurement points in a row, so we can use matrix
+    # multiplication with the @ operator. To keep track of things, let's create the 1-D array that 
+    # identifies the profiles' serial numbers, rigs, events and sites.
+    # First, the SN (identical for all measurement points within a profile)
+    if sns is None:
+        sns = [None] * n_profiles
+    sns_inds = np.array([[sns[p_ind]] * n_steps for p_ind in range(n_profiles)]).ravel()
+    # idem for the rigs
+    if rigs is None:
+        rigs = [None] * n_profiles
+    rigs_inds = np.array([[rigs[p_ind]] * n_steps for p_ind in range(n_profiles)]).ravel()
+    # idem for the events
+    if evts is None:
+        evts = [None] * n_profiles
+    evts_inds = np.array([[evts[p_ind]] * n_steps for p_ind in range(n_profiles)]).ravel()
+    # idem for the sites
+    if sites is None:
+        sites = [None] * n_profiles
+    sites_inds = np.array([[sites[p_ind]] * n_steps for p_ind in range(n_profiles)]).ravel()
+
+    # Let's also keep track of the original indices of the data
+    i_inds = np.array(n_profiles * list(range(n_steps))).ravel()
+
+    # Next, I follow Barlow p.60. Only I do it per layer k, and I only keep the profile points
+    # that are included in the construction of the layer. This keeps the matrices small by
+    # avoiding a lot of 0's.
+    # Let's now start looping through all the layers
+    for k_ind in range(n_steps_new): # loop through all the new layers
+
+        # First, what are the limiting level indices of this layer ?
+        j_min = np.min(old_inds[k_ind])
+        j_max = np.max(old_inds[k_ind]) + 1
+
+        # What are the indices of the in-layer points ?
+        in_layer = (i_inds >= j_min) * (i_inds < j_max)
+
+        if len(in_layer[in_layer]) != binning * n_profiles:
+            raise Exception('Ouch! This error is impossible.')
+
+        # First, build the G matrix
+        if method == 'weighted mean':
+            G_mat = w_ps.ravel()[in_layer]/w_ms[k_ind]
+        elif method == 'delta':
+            G_mat = np.append(1/binning * np.ones(binning), -1/binning * np.ones(binning))
+
+        # Very well, the covariance matrix V is the sum of the different uncertainty components:
+        # uncorrelated, environmental-correlated, spatial-correlated, temporal-correlated
+        # Let's assemble the uncertainties of this specific step
+        local_sigma_us = np.array([sigma[j_min:j_max] for sigma in sigma_us]).ravel()
+        local_sigma_es = np.array([sigma[j_min:j_max] for sigma in sigma_es]).ravel()
+        local_sigma_ss = np.array([sigma[j_min:j_max] for sigma in sigma_ss]).ravel()
+        local_sigma_ts = np.array([sigma[j_min:j_max] for sigma in sigma_ts]).ravel()
+
+        # Let us now assemble the U matrices, filling all the cross-correlations for the different
+        # types of uncertainties
+        U_mats = []
+        for (sigma_name, sigma_vals) in [('sigma_u', local_sigma_us),
+                                         ('sigma_e', local_sigma_es),
+                                         ('sigma_s', local_sigma_ss),
+                                         ('sigma_t', local_sigma_ts),
+                                        ]:
+            U_mat = corcoef_gdps(np.tile(i_inds[in_layer], (binning * n_profiles, 1)), # i
+                                 np.tile(i_inds[in_layer], (binning * n_profiles, 1)).T, # j
+                                 sigma_name,
+                                 sn_i=np.tile(sns_inds[in_layer], (binning * n_profiles, 1)),
+                                 sn_j=np.tile(sns_inds[in_layer], (binning * n_profiles, 1)).T,
+                                 rig_i=np.tile(rigs_inds[in_layer], (binning * n_profiles, 1)),
+                                 rig_j=np.tile(rigs_inds[in_layer], (binning * n_profiles, 1)).T,
+                                 event_i=np.tile(evts_inds[in_layer], (binning * n_profiles, 1)),
+                                 event_j=np.tile(evts_inds[in_layer], (binning * n_profiles, 1)).T,
+                                 site_i=np.tile(sites_inds[in_layer], (binning * n_profiles, 1)),
+                                 site_j=np.tile(sites_inds[in_layer], (binning * n_profiles, 1)).T,
+                                )
+            # Implement the multiplication. Mind the structure of these arrays to get the correct
+            # mix of Hadamard and dot products where I need them !
+            U_mat = np.multiply(U_mat, np.array([sigma_vals]).T @ np.array([sigma_vals]))
+            U_mats += [U_mat]
+
+            # Include a sanity check: if U_mat is not an identity matrix for sigma_u,
+            # something is very wrong.
+            if sigma_name == 'sigma_u' and np.any(U_mat != np.identity(binning * n_profiles)):
+                raise Exception('Ouch ! Something is very wrong here.')
+
+        # Here, I can finally use matrix multiplication (using @) to save me a lot of convoluted 
+        # sums ...
+        # If I only have nan's then that's my result. If I only have partial nan's, then make sure I
+        # still get a number out.
+        if np.all(np.isnan(G_mat)):
+            sigma_us_new[k_ind] = np.nan
+            sigma_es_new[k_ind] = np.nan
+            sigma_ss_new[k_ind] = np.nan
+            sigma_ts_new[k_ind] = np.nan
+
+        else:
+
+            # Replace all the nan's with zeros so they do not intervene in the sums
+            sigma_us_new[k_ind] = np.where(np.isnan(G_mat), 0, G_mat) @ \
+                                  np.where(np.isnan(U_mats[0]), 0, U_mats[0]) @ \
+                                  np.where(np.isnan(G_mat.T), 0, G_mat.T)
+            sigma_us_new[k_ind] = np.sqrt(sigma_us_new[k_ind])
+
+            sigma_es_new[k_ind] = np.where(np.isnan(G_mat), 0, G_mat) @ \
+                                  np.where(np.isnan(U_mats[1]), 0, U_mats[1]) @ \
+                                  np.where(np.isnan(G_mat.T), 0, G_mat.T)
+            sigma_es_new[k_ind] = np.sqrt(sigma_es_new[k_ind])
+
+            sigma_ss_new[k_ind] = np.where(np.isnan(G_mat), 0, G_mat) @ \
+                                  np.where(np.isnan(U_mats[2]), 0, U_mats[2]) @ \
+                                  np.where(np.isnan(G_mat.T), 0, G_mat.T)
+            sigma_ss_new[k_ind] = np.sqrt(sigma_ss_new[k_ind])
+
+            sigma_ts_new[k_ind] = np.where(np.isnan(G_mat), 0, G_mat) @ \
+                                  np.where(np.isnan(U_mats[3]), 0, U_mats[3]) @ \
+                                  np.where(np.isnan(G_mat.T), 0, G_mat.T)
+            sigma_ts_new[k_ind] = np.sqrt(sigma_ts_new[k_ind])
+
+    return (x_ms, sigma_us_new, sigma_es_new, sigma_ss_new, sigma_ts_new, old_inds, new_inds)
--- test/test_gruan/test_gruan.py
+++ test/test_gruan/test_gruan.py
@@ -13,7 +13,7 @@ Module contents: Testing classes and function of the gruan submodule.
 # Import from python packages and modules
 import numpy as np
 import pytest
-from dvas.gruan import corcoef_gdp
+from dvas.gruan import corcoef_gdps, merge_andor_rebin_gdps
 
 # Define a series of reference measurement parameters, to test the different combination options.
 T_OK = [np.zeros(1), np.zeros(1)]                        # Same time
@@ -29,7 +29,7 @@ SIT_NOK = [np.array(['site 1']), np.array(['site 2'])]   # Different site
 
 
 # Let us test a series of conditions for the different types of uncertainty types
-@pytest.mark.parametrize("test_input, expected",
+@pytest.mark.parametrize("test_input_1, expected_1",
                          [   # Uncorrelated errors: same point
                              (T_OK + ['sigma_u'] + SN_OK + RIG_OK + EVT_OK + SIT_OK, 1),
                              # Uncorrelated errors: different time
@@ -71,7 +71,7 @@ SIT_NOK = [np.array(['site 1']), np.array(['site 2'])]   # Different site
                              (T_NOK + ['sigma_t'] + SN_NOK + RIG_OK + EVT_OK + SIT_NOK, 1),
                          ])
 
-def test_gruan_corcoef_gdp(test_input, expected):
+def test_gruan_corcoef_gdp(test_input_1, expected_1):
     """Function used to test if the GDP correlations are properly implemented.
 
     The function tests:
@@ -80,8 +80,66 @@ def test_gruan_corcoef_gdp(test_input, expected):
     """
 
     # If I get here, then most likely it is all working fine.
-    assert corcoef_gdp(test_input[0], test_input[1], test_input[2],
-                       sn_i=test_input[3], sn_j=test_input[4],
-                       rig_i=test_input[5], rig_j=test_input[6],
-                       event_i=test_input[7], event_j=test_input[8],
-                       site_i=test_input[9], site_j=test_input[10]) == expected
+    assert corcoef_gdps(test_input_1[0], test_input_1[1], test_input_1[2],
+                       sn_i=test_input_1[3], sn_j=test_input_1[4],
+                       rig_i=test_input_1[5], rig_j=test_input_1[6],
+                       event_i=test_input_1[7], event_j=test_input_1[8],
+                       site_i=test_input_1[9], site_j=test_input_1[10]) == expected_1
+
+# Let us test a series of conditions for the different types of uncertainty types
+@pytest.mark.parametrize("test_input_2, expected_2",
+                         [   # Delta from two profiles from the same rig/event/site
+                             # Sigma_s and sigma_t cancel perfectly. 
+                             (SN_NOK + RIG_OK + EVT_OK + SIT_OK + [1, 'delta'],
+                              (np.ones(5), np.sqrt(2)*np.ones(5), None, np.zeros(5), np.zeros(5),
+                              [np.array([i]) for i in range(5)], range(5))),
+                         ])
+
+def test_gruan_merge_andor_rebin_gdps(test_input_2, expected_2):
+    """Function used to test if the routine combining GDP profiles is ok.
+
+    The function tests:
+        - correct propagation of errors
+
+    Note:
+       Inspired, in part, from `this post 
+       <https://stackoverflow.com/questions/39896716/can-i-perform-multiple-assertions-in-pytest>`__
+       by Jon Clements.
+
+    Todo:
+        * expand lists of tests to all uncertainty types.
+
+    """
+    errors = []
+
+    out = merge_andor_rebin_gdps([np.ones(5), np.zeros(5)], # profiles
+                                 [np.ones(5), np.ones(5)],  #sigma_us
+                                 [np.ones(5), np.ones(5)], #sigma_es
+                                 [np.ones(5), np.ones(5)], #sigma_ss
+                                 [np.ones(5), np.ones(5)], #sigma_ts
+                                 sns=test_input_2[:2],
+                                 rigs=test_input_2[2:4],
+                                 evts=test_input_2[4:6],
+                                 sites=test_input_2[6:8],
+                                 binning=test_input_2[8],
+                                 method=test_input_2[9])
+
+    if np.all(out[0] != expected_2[0]):
+        errors += ['Merged profile is wrong']
+    if np.all(out[1] != expected_2[1]):
+        errors += ['sigma_us_new is wrong']
+    # TODO: sigma_es
+    if np.all(out[3] != expected_2[3]):
+        errors += ['sigma_ss_new is wrong']
+    if np.all(out[4] != expected_2[4]):
+        errors += ['sigma_ts_new is wrong']
+    if np.all(out[5] != expected_2[5]):
+        errors += ['old_inds is wrong']
+    if np.all(out[6] != expected_2[6]):
+        errors += ['new_ind is wrong']
+
+    import pdb
+    pdb.set_trace()
+
+    assert not errors
+                                  
