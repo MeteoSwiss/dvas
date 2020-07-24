c9c5bd786cb9d366cd7ef3cd680eca1e97556450
fpavogt <fpavogt@users.noreply.github.com>
2020-07-24 16:04:31 +0200

    Prepare gruan.stats submodule

---
+++ src/dvas/gruan/stats.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+
+Copyright (c) 2020 MeteoSwiss, contributors listed in AUTHORS.
+
+Distributed under the terms of the GNU General Public License v3.0 or later.
+
+SPDX-License-Identifier: GPL-3.0-or-later
+
+This module contains GRUAN-related stats routines.
+
+"""
+
+
+# Run a KS test between two GDP profiles
+def ks_test():
+
+    return False
+
+# Run a chi-square analysis between a merged profile and its components
+def chi_square():
+
+    return False
