4a677b39ae53de29d9a6513c140dfb36df764b2c
fpavogt <fpavogt@users.noreply.github.com>
2020-07-30 16:10:33 +0200

    Set the proper style when loading the plot submodule

--- src/dvas/plot/__init__.py
+++ src/dvas/plot/__init__.py
@@ -7,4 +7,18 @@ SPDX-License-Identifier: GPL-3.0-or-later
 
 """
 
+# Import from Python packages
+import matplotlib.pyplot as plt
+
+# Import from this package
 from .plot import *
+
+from ..dvas_environ import path_var as env_path_var
+from .. import pkg_path
+from . import plot_utils as pu
+
+# Immediately enable the base look for DVAS plots, as soon as we load the module.
+plt.style.use(str(Path(pkg_path, 'plot', 'mpl_styles', pu.PLOT_STYLES['base'])))
+
+# Uncomment the following for having the pretty LaTeX plots by default
+#plt.style.use(str(Path(pkg_path, 'plot', 'mpl_styles', pu.PLOT_STYLES['latex'])))
