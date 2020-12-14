53c147b3469ae99b9401411b558e31e6b4a9640f
fpavogt <fpavogt@users.noreply.github.com>
2020-12-14 15:22:49 +0100

    Fix doc_aut_publish bug

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -2,9 +2,18 @@
 # Created June 2020; F.P.A. Vogt; frederic.vogt@meteoswiss.ch
 #
 
-# Step 0: just clean the existing apidoc rst files and any previous build folder
-rm -rf ./source/modules
-rm -rf ./build
+# Step 0: clean the existing apidoc rst files and any previous build folder.
+# Make sure to not delete any important .git info for the CI auto_publish action !
+#rm -rf ./source/modules
+#rm -rf ./build/*.html
+#rm -rf ./build/_sources
+#rm -rf ./build/_images
+#rm -rf ./build/_static
+#rm -rf ./build/.buildinfo
+#rm -rf ./build/.doctrees
+cd ./build
+find . -not \( -path '*.git*' -o -name '.nojekyll' -o -name '.-gitignore' \) -delete
+cd ..
 
 # Step 1: run autodoc to generate all the docstring rst files.
 # Force the rewrite of all of them to capture *all* the recent changes.
