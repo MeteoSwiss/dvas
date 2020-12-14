b61c7b068e03d43f057c5c881bdaee29dc2b2fd2
fpavogt <fpavogt@users.noreply.github.com>
2020-12-14 16:10:26 +0100

    Fix new bug

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -4,16 +4,14 @@
 
 # Step 0: clean the existing apidoc rst files and any previous build folder.
 # Make sure to not delete any important .git info for the CI auto_publish action !
-#rm -rf ./source/modules
+rm -rf ./source/modules
 #rm -rf ./build/*.html
 #rm -rf ./build/_sources
 #rm -rf ./build/_images
 #rm -rf ./build/_static
 #rm -rf ./build/.buildinfo
 #rm -rf ./build/.doctrees
-cd ./build
-find . -not \( -path '*.git*' -o -name '.nojekyll' -o -name '.-gitignore' \) -delete
-cd ..
+find ./build -not \( -path '*.git*' -o -name '.nojekyll' -o -name '.-gitignore' \) -delete
 
 # Step 1: run autodoc to generate all the docstring rst files.
 # Force the rewrite of all of them to capture *all* the recent changes.
