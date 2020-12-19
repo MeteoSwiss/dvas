d3c5881aba768c303c7852ddbbffff3ea637eb47
“Gonzague <gonzague.romanens@meteoswiss.ch>
2020-12-19 16:11:33 +0100

    add authors and changelog line organize flights into yml files to be more explicit

--- examples/SampleDatasetMultipayload.py
+++ examples/SampleDatasetMultipayload.py
@@ -18,11 +18,9 @@ dvas_path_var.orig_data_path = Path(__file__).parent / 'data'
 dvas_path_var.config_dir_path = Path(__file__).parent / 'config'
 
 # Import
-from dvas.data.data import MultiProfile, MultiRSProfile, MultiGDPProfile
 from dvas.data.data import update_db
 from dvas.database.database import DatabaseManager
 
-from dvas.errors import dvasError
 from dvas.logger import init_log as dvas_init_log
 
 #import dvas.tools.gruan as dtg
