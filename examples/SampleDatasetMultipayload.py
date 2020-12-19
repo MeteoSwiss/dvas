b6a0bda42aa6830c1256b8a8cdac8bf35b4b650a
“Gonzague <gonzague.romanens@meteoswiss.ch>
2020-12-19 17:04:50 +0100

    comment import for pylint

--- examples/SampleDatasetMultipayload.py
+++ examples/SampleDatasetMultipayload.py
@@ -12,29 +12,26 @@ from pathlib import Path
 
 # Set the data path to look where we currently are
 from dvas.environ import path_var as dvas_path_var
-from dvas.environ import log_var as dvas_log_var
 # WARNING: this must be done BEFORE importing anything else ... !
 dvas_path_var.orig_data_path = Path(__file__).parent / 'data'
 dvas_path_var.config_dir_path = Path(__file__).parent / 'config'
 
 # Import
-from dvas.data.data import update_db
+from dvas.data.data import MultiProfile, MultiRSProfile, MultiGDPProfile
+from dvas.data.io import update_db
 from dvas.database.database import DatabaseManager
 
-from dvas.logger import init_log as dvas_init_log
+from dvas.errors import dvasError
 
 #import dvas.tools.gruan as dtg
 import dvas.plots.utils as dpu
 
+from dvas.dvas import start_log
 
 if __name__ == '__main__':
 
-    # Set the log level we want
-    dvas_log_var.log_level = 'DEBUG' # 'INFO' is the default.
-    dvas_log_var.log_mode = 'CONSOLE' # 'FILE' if you want a live update
-
     # Start the logging
-    dvas_init_log()
+    start_log(3)
 
     # Let us fine-tune the plotting behavior of dvas
     dpu.set_mplstyle('nolatex') # The safe option. Use 'latex' fo prettier plots.
@@ -50,7 +47,7 @@ if __name__ == '__main__':
 
     # Define some search queries
     filt_gdp = "tag('gdp')"
-    filt_dt = "dt('20170712T000000Z', '==')"
+    filt_dt = "dt('20180125T120000Z', '==')"
     filt_vof = "and_(%s,%s)" % (filt_gdp, filt_dt)
     filt_gdp_der = "and_(tag('derived'), tag('gdp'))"
     filt_raw = "and_(tag('raw'), not_(tag('gdp')))"
@@ -63,10 +60,9 @@ if __name__ == '__main__':
 
     # Update the database + log
     if RESET_DB:
-        #with LogManager():
-            update_db('tdtpros1', strict=True)
-            update_db('trepros1', strict=True)
-            update_db('treprosu_r', strict=True)
-            update_db('treprosu_s', strict=True)
-            update_db('treprosu_t', strict=True)
-            update_db('altpros1', strict=True)
+        update_db('tdtpros1', strict=True)
+        update_db('trepros1', strict=True)
+        update_db('treprosu_r', strict=True)
+        update_db('treprosu_s', strict=True)
+        update_db('treprosu_t', strict=True)
+        update_db('altpros1', strict=True)
