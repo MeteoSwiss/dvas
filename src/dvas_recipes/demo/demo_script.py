f7a30ca35c4b015e797c49269efe642605cf3f01
13974615+fpavogt <13974615+fpavogt@users.noreply.github.com>
2021-02-05 14:43:55 +0100

    Cleanup demo script

--- src/dvas_recipes/demo/demo_script.py
+++ src/dvas_recipes/demo/demo_script.py
@@ -17,21 +17,18 @@ from dvas.environ import path_var as dvas_path_var
 dvas_path_var.orig_data_path = Path(__file__).parent / 'data'
 dvas_path_var.config_dir_path = Path(__file__).parent / 'config'
 
-# Import
+# Import stuff from dvas
+import dvas.plots.utils as dpu
+from dvas.dvas import start_log
 from dvas.data.data import MultiProfile, MultiRSProfile, MultiGDPProfile
 from dvas.data.io import update_db
 from dvas.database.database import DatabaseManager
 from dvas.errors import dvasError
 
-#import dvas.tools.gruan as dtg
-import dvas.plots.utils as dpu
-
-from dvas.dvas import start_log
-
 if __name__ == '__main__':
 
     # Start the logging
-    start_log(3)
+    start_log(3) # 3 = log to screen only.
 
     # Let us fine-tune the plotting behavior of dvas
     dpu.set_mplstyle('nolatex') # The safe option. Use 'latex' fo prettier plots.
@@ -45,17 +42,17 @@ if __name__ == '__main__':
     # Reset the DB to "start fresh" ?
     RESET_DB = True
 
-    # Define some search queries
+    # Define some basic search queries
     filt_gdp = "tags('gdp')"
-    filt_dt = "dt('20171024T120000Z', '==')"
-    filt_vof = "and_(%s,%s)" % (filt_gdp, filt_dt)
-    filt_gdp_der = "and_(tags('derived'), tags('gdp'))"
-    filt_raw = "and_(tags('raw'), not_(tags('gdp')))"
+    filt_raw = "tags('raw')"
+    filt_raw_not = "not_(tags('raw'))"
     filt_all = "all()"
-    filt_der = "and_(tags('derived'), not_(tags('gdp')))"
-    filt_cws = "tags('cws')"
+    filt_dt = "dt('20171024T120000Z', '==')"
 
-    # Create the database
+    # Define some more complex queries
+    filt_gdp_dt = "and_(%s, %s)" % (filt_gdp, filt_dt)
+
+    # Create the dvas database
     db_mngr = DatabaseManager(reset_db=RESET_DB)
 
     # Update the database + log
@@ -67,21 +64,15 @@ if __name__ == '__main__':
         update_db('treprosu_t', strict=True)
         update_db('altpros1', strict=True)
 
-
     # Load a basic profile, with a variable, and altitude.
     prf = MultiProfile()
     prf.load_from_db(filt_raw, 'trepros1', 'altpros1')
 
     # Load a basic time profile, with a variable and altitude
     rs_prf = MultiRSProfile()
-    try:
-        rs_prf.load_from_db(filt_der, 'trepros1', 'tdtpros1', alt_abbr='altpros1')
-
-    except dvasError:
-        rs_prf.load_from_db(filt_raw, 'trepros1', 'tdtpros1', alt_abbr='altpros1')
-        rs_prf.sort()
-        #rs_prf.resample()
-        rs_prf.save_to_db()
+    rs_prf.load_from_db(filt_dt, 'trepros1', 'tdtpros1', alt_abbr='altpros1')
+    rs_prf.sort()
+    rs_prf.save_to_db()
 
     # Acccess some useful info about the data
     print(rs_prf.get_info('evt_id'))
@@ -91,25 +82,10 @@ if __name__ == '__main__':
 
     # Load GDPs for temperature, including all the errors
     gdp_prfs = MultiGDPProfile()
-    gdp_prfs.load_from_db(filt_vof, 'trepros1', alt_abbr='altpros1', tdt_abbr='tdtpros1',
+    gdp_prfs.load_from_db(filt_gdp_dt, 'trepros1', alt_abbr='altpros1', tdt_abbr='tdtpros1',
                           ucr_abbr='treprosu_r', ucs_abbr='treprosu_s', uct_abbr='treprosu_t',
                           inplace=True)
 
     # Let us inspect the profiles with dedicated plots.
     gdp_prfs.plot(fn_prefix='01') # Defaults behavior, just adding a prefix to the filename.
     gdp_prfs.plot(uc='tot', show_plt=True, fmts=[]) # Now with errors. Show it but don't save it.
-
-
-    # Compute a working standard
-    # TODO: disabled in the example for now. We must first synchronize the Profiles.
-    # out = dtg.combine_gdps(gdp_prfs, binning=1, method='weighted mean')
-    # For the sake of the exemple, save this to the database
-    #out.save_to_db()
-
-    # And re-load it.
-    #cws_prf = MultiGDPProfile()
-    #cws_prf.load_from_db(filt_cws, 'trepros1', alt_abbr='altpros1', tdt_abbr='tdtpros1',
-    #                     ucr_abbr='treprosu_r', ucs_abbr='treprosu_s', uct_abbr='treprosu_t',
-    #                     inplace=True)
-    #
-    #dpg.gdps_vs_cws(gdp_prfs, cws_prf)
