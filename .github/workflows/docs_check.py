6be4cbac399f2c53458fe7c0b31c1dac26ff60a3
13974615+fpavogt <13974615+fpavogt@users.noreply.github.com>
2021-02-08 17:02:09 +0100

    Normalize copyright notices.

--- .github/workflows/docs_check.py
+++ .github/workflows/docs_check.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 '''
+Copyright (c) 2020 MeteoSwiss, contributors listed in AUTHORS.
+
+Distributed under the terms of the GNU General Public License v3.0 or later.
+
+SPDX-License-Identifier: GPL-3.0-or-later
+
 This script can be used together with a Github Action to build the docs, and check for errors and
 warnings. This script assumes that one is located within the "docs" folder.
 
