03c2452c7c1f93bc422e272474ae538ec0668be9
13974615+fpavogt <13974615+fpavogt@users.noreply.github.com>
2021-03-05 16:52:44 +0100

    WIP

--- test/dvas/test_tools/test_gdps/test_gdps.py
+++ test/dvas/test_tools/test_gdps/test_gdps.py
@@ -13,18 +13,81 @@ Module contents: Testing 'gruan' classes and function of the tools submodule.
 # Import from python packages and modules
 import numpy as np
 import pytest
+import pandas as pd
+
+from dvas.data.strategy.data import GDPProfile
+from dvas.data.data import MultiGDPProfile
+from dvas.database.database import InfoManager
+
+# Functions to test
 from dvas.tools.gdps import combine
 
+# Define db_data. This is some black magic that is directly related to conftest.py
+# This is a temporary db, that is required for get_info() to work properly with mdl_id.
+# It all relies on the db config files located in the processing arena
+db_data = {
+    'sub_dir': 'test_tool_gdps',
+    'data': [{'mdl_name': 'AR-GDP_001',
+              'srn': 'AR1',
+              'pid': '0',},
+             {'mdl_name': 'BR-GDP_001',
+              'srn': 'BR1',
+              'pid': '1',},
+             {'mdl_name': 'BR-GDP_001',
+              'srn': 'CR1',
+              'pid': '2',},
+            ]}
+
+
+
+@pytest.fixture
+def gdp_1_prfs(db_init):
+    """ Return a MultiGDPProfile with 1 GDPprofiles in it. """
+
+    # Get the oids from the profiles
+    oids = [item['oid'] for item in db_init.data]
+
+    # Prepare some datasets to play with
+    info_1 = InfoManager('20210302T0000Z', oids[0], tags=['e:1', 'r:1'])
+    data_1 = pd.DataFrame({'alt': [10., 15., 20.], 'val': [10., 20., 30.], 'flg': [1, 1, 1],
+                           'tdt': [1, 2, 3], 'ucr': [1, 1, 1], 'ucs': [1, 1, 1], 'uct': [1, 1, 1],
+                           'ucu': [1, 1, 1]})
+
+    # Let's build a multiprofile so I can test things out.
+    multiprf = MultiGDPProfile()
+    multiprf.update({'val': None, 'tdt': None, 'alt': None, 'flg': None, 'ucr': None, 'ucs': None,
+                     'uct': None, 'ucu': None},
+                    [GDPProfile(info_1, data_1)])
+
+    return multiprf
+
+@pytest.fixture
+def gdp_2_prfs(db_init):
+    """ Return a MultiGDPProfile with 2 GDPprofiles in it. """
+
+    # Get the oids from the profiles
+    oids = [item['oid'] for item in db_init.data]
+
+    # Prepare some datasets to play with
+    info_1 = InfoManager('20210302T0000Z', oids[0], tags=['e:1', 'r:1'])
+    data_1 = pd.DataFrame({'alt': [10., 15., 20.], 'val': [10., 20., 30.], 'flg': [1, 1, 1],
+                           'tdt': [1, 2, 3], 'ucr': [1, 1, 1], 'ucs': [1, 1, 1], 'uct': [1, 1, 1],
+                           'ucu': [1, 1, 1]})
+    info_2 = InfoManager('20210302T0000Z', oids[1], tags=['e:1', 'r:1'])
+    data_2 = pd.DataFrame({'alt': [11., 15., 20.1], 'val': [10., 21., np.nan], 'flg': [1, 1, 1],
+                           'tdt': [1, 2, 3], 'ucr': [1, 1, 1], 'ucs': [1, 1, 1], 'uct': [1, 1, 1],
+                           'ucu': [1, 1, 1]})
+
+    # Let's build a multiprofile so I can test things out.
+    multiprf = MultiGDPProfile()
+    multiprf.update({'val': None, 'tdt': None, 'alt': None, 'flg': None, 'ucr': None, 'ucs': None,
+                     'uct': None, 'ucu': None},
+                    [GDPProfile(info_1, data_1), GDPProfile(info_2, data_2)])
+
+    return multiprf
+
 # Let us test a series of conditions for the different types of uncertainty types
-@pytest.mark.parametrize("test_input_2, expected_2", [
-    # Weighted mean binning of a single profile
-    ([np.array([1, 2])] * 5 + [2],
-     (np.array([1.2]), np.sqrt(112/25), [[0, 1]], np.array([0.5]))),
-    # Idem, but with NaNs
-    ([np.array([np.nan, 2])] * 5 + [2],
-     (np.array([2]), 4, [[0, 1]], np.array([0.5]))),
-    ])
-def test_combine_gdps(test_input_2, expected_2):
+def test_combine(gdp_1_prfs, gdp_2_prfs):
     """Function used to test if the routine combining GDP profiles is ok.
 
     The function tests:
@@ -36,88 +99,18 @@ def test_combine_gdps(test_input_2, expected_2):
        by Jon Clements.
 
     """
-    #errors = []
 
-    #out = merge_andor_rebin_gdps([test_input_2[0]], # profiles
-    #                             [test_input_2[1]], # sigma_us
-    #                             [test_input_2[2]], # sigma_es
-    #                             [test_input_2[3]], # sigma_ss
-    #                             [test_input_2[4]], # sigma_ts
-    #                             srns=None,
-    #                             mods=None,
-    #                             rigs=None,
-    #                             evts=None,
-    #                             sits=None,
-    #                             binning=test_input_2[5],
-    #                             method='weighted mean')
+    # 0) Ultra-basic test: the mean of a single profile with binning = 1 should return the
+    # same thing
 
-    #if np.all(out[0] != expected_2[0]):
-    #    errors += ['Merged profile is wrong']
-    #if np.all(np.sqrt(out[1]**2 + out[2]**2 + out[3]**2 + out[4]**2) != expected_2[1]):
-    #    errors += ['sigma_tot is wrong']
-    #if np.all(out[5] != expected_2[2]):
-    #    errors += ['old_inds is wrong']
-    #if np.all(out[6] != expected_2[3]):
-    #    errors += ['new_ind is wrong']
+    for method in ['mean', 'weighted mean']:
+        out = combine(gdp_1_prfs, binning=1, method=method, chunk_size=200, n_cpus='max')
 
-    #assert not errors
-    assert True
+        import pdb
+        pdb.set_trace()
 
-# Let us test a series of conditions for the different types of uncertainty types
-@pytest.mark.parametrize("test_input_3, expected_3", [
-    # Delta from two profiles from the same rig/event/site
-    # Sigma_s and sigma_t cancel perfectly.
-    (SRN_NOK + MDL_OK + RIG_OK + EVT_OK + [1, 'delta'],
-     (np.ones(5), np.sqrt(2)*np.ones(5), None, np.zeros(5), np.zeros(5),
-      [np.array([i]) for i in range(5)], range(5))),
-    # Delta + binning from two profiles from the same rig/event/site
-    (SRN_NOK + MDL_OK + RIG_OK + EVT_OK + [3, 'delta'],
-     (np.ones(2), np.array([np.sqrt(6)/3, np.sqrt(4)/2]), None, np.zeros(2), np.zeros(2),
-      [[0, 1, 2], [3, 4]], np.array([1, 3.5]))),
-    ])
-def test_combine_gdps_bis(test_input_3, expected_3):
-    """Function used to test if the routine combining GDP profiles is ok.
-
-    The function tests:
-        - correct propagation of errors
-
-    Note:
-       Inspired, in part, from `this post
-       <https://stackoverflow.com/questions/39896716/can-i-perform-multiple-assertions-in-pytest>`__
-       by Jon Clements.
-
-    Todo:
-        * expand lists of tests to all uncertainty types.
-
-    """
-    #errors = []
-
-    #out = merge_andor_rebin_gdps([np.ones(5), np.zeros(5)], # profiles
-    #                             [np.ones(5), np.ones(5)],  #sigma_us
-    #                             [np.ones(5), np.ones(5)], #sigma_es
-    #                             [np.ones(5), np.ones(5)], #sigma_ss
-    #                             [np.ones(5), np.ones(5)], #sigma_ts
-    #                             srns=test_input_3[:2],
-    #                             mods=test_input_3[2:4],
-    #                             rigs=test_input_3[4:6],
-    #                             evts=test_input_3[6:8],
-    #                             sits=test_input_3[8:10],
-    #                             binning=test_input_3[10],
-    #                             method=test_input_3[11])
-
-    #if np.all(out[0] != expected_3[0]):
-    #    errors += ['Merged profile is wrong']
-    #if np.all(out[1] != expected_3[1]):
-    #    errors += ['sigma_us_new is wrong']
-    # TODO: sigma_es
-    #if np.all(out[3] != expected_3[3]):
-    #    errors += ['sigma_ss_new is wrong']
-    #if np.all(out[4] != expected_3[4]):
-    #    errors += ['sigma_ts_new is wrong']
-    #if np.all(out[5] != expected_3[5]):
-    #    errors += ['old_inds is wrong']
-    #if np.all(out[6] != expected_3[6]):
-    #    errors += ['new_ind is wrong']
-
-    #assert not errors
-    assert True
+        for key in ['val', 'ucr', 'ucs', 'uct', 'ucu']:
+            assert np.array_equal(out.profiles[0].data[key], gdp_1_prfs.profiles[0].data[key])
+        for key in ['tdt', 'alt']:
+            assert np.array_equal(out.profiles[0].data.index.get_level_values(key),
+                                  gdp_1_prfs.profiles[0].data.index.get_level_values(key))
