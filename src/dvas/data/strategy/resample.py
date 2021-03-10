7eef48b02cbc64efede22660213b83ef15468cc5
13974615+fpavogt <13974615+fpavogt@users.noreply.github.com>
2021-03-10 08:36:55 +0100

    Keep a single Resample strategy

--- src/dvas/data/strategy/resample.py
+++ src/dvas/data/strategy/resample.py
@@ -23,86 +23,20 @@ from ...hardcoded import PRF_REF_INDEX_NAME, PRF_REF_TDT_NAME, PRF_REF_ALT_NAME,
 from ...hardcoded import PRF_REF_VAL_NAME, PRF_REF_UCR_NAME, PRF_REF_UCS_NAME, PRF_REF_UCT_NAME
 from ...hardcoded import PRF_REF_UCU_NAME
 
-
-class ResampleRSStrategy(MPStrategyAC):
-    """Class to manage the (time) resampling of Profiles"""
-
-    def execute(self, prfs, freq='1s'):
-        """ Implementation of time resampling method for RSProfiles (i.e. no error propagation).
-
-        Args:
-            prfs (list of RSProfiles): RS Profiles to resample.
-            freq (str, optional): see pandas.timedelta_range(). Defaults to '1s'.
-
-        Returns:
-            dvas.data.MultiRSProfile|MultiGDPProfile: the resampled MultiProfile.
-
-        """
-
-        # Some sanity checks to begin with
-        if not isinstance(prfs, list):
-            raise DvasError("Ouch ! prfs should be of type list, and not: {}".format(type(prfs)))
-        # The following should in principle never happen because the strategy ensures that.
-        # If this blows up, then something must have gone really wrong ...
-        if any([PRF_REF_TDT_NAME not in prf.get_index_attr() for prf in prfs]):
-            raise DvasError("Ouch ! I can only resample profiles with a timedelta array ...")
-
-        # Very well, let's start looping and resampling each Profile
-        for (prf_ind, prf) in enumerate(prfs):
-
-            # Let's identify the min and max integer values, rounded to the nearest second.
-            t_0 = min(prf.data.index.get_level_values(PRF_REF_TDT_NAME)).ceil('1s')
-            t_1 = max(prf.data.index.get_level_values(PRF_REF_TDT_NAME)).floor('1s')
-
-            # Turn this into a regular grid
-            new_tdt = pd.timedelta_range(t_0, t_1, freq=freq, name=PRF_REF_TDT_NAME)
-            # Get the existing time steps
-            old_tdt = prf.data.index.get_level_values(PRF_REF_TDT_NAME)
-
-            # Check if the new index is the same as the old. And if so, continue to the next
-            # next profile without changing anything
-            if len(new_tdt) == len(prf.data):
-                if all(new_tdt == prf.data.index.get_level_values(PRF_REF_TDT_NAME)):
-                    continue
-
-            # Get ready to interpolate.
-            # First, get the original data out, reseting all the indices to columns.
-            this_data = prf.data.reset_index()
-
-            # Let's drop the original integer index, to avoid type conversion issues
-            this_data.drop(columns=PRF_REF_INDEX_NAME, inplace=True)
-
-            # Create a new dataframe to keep the interpolated stuff
-            new_data = pd.DataFrame(new_tdt, columns=this_data.columns)
-
-            # Loop through the different columns
-            for name in this_data.columns:
-
-                if name == PRF_REF_FLG_NAME:
-                    # TODO: deal with the flags
-                    continue
-
-                func = interp1d(old_tdt.values.astype('int64'),
-                                this_data.loc[:, name].values, kind='linear')
-
-                new_data.loc[:, name] = func(new_tdt.values.astype('int64'))
-
-            # And finally let's assign the new DataFrame to the Profile. The underlying setter
-            # will take care of reformatting all the indices as needed.
-            prfs[prf_ind].data = new_data
-
-            return prfs
-
-class ResampleGDPStrategy(MPStrategyAC):
-    """ Class to handle the resample strategy for GDPs, incl.full error propagation.
-
-    """
+class ResampleStrategy(MPStrategyAC):
+    """ Class to handle the resample strategy for RS and GDPs Profiles. """
 
     def execute(self, prfs, freq='1s'):
-        """Implementation of time resampling method for GDP Profiles.
+        """Implementation of time resampling method for RS and GDP Profiles.
+
+        .. note::
+
+            This strategy does NOT treat NaN's in a special way. This implies that if a NaN is one
+            of the two closest original data points from a new location to be interpolated, that
+            location will result in a NaN as well.
 
         Args:
-            prfs (list of GDPProfiles): GDP Profiles to resample.
+            prfs (list of RSProfiles|GDPProfiles): GDP Profiles to resample.
             freq (str, optional): see pandas.timedelta_range(). Defaults to '1s'.
 
         Returns:
@@ -148,7 +82,8 @@ class ResampleGDPStrategy(MPStrategyAC):
             x_ip1_ind = [this_data[PRF_REF_TDT_NAME].searchsorted(xi_star,
                          side='right') for xi_star in new_tdt]
 
-            # Compute the weights for each point
+            # ---- This is useful only for GDP Profiles ----
+            # Compute the Jacobian matrix
             w_vals = [(item-old_tdt.values[x_ip1_ind[ind]-1])/
                       np.diff(old_tdt.values)[x_ip1_ind[ind]-1]
                       for (ind, item) in enumerate(new_tdt.values)]
@@ -160,6 +95,7 @@ class ResampleGDPStrategy(MPStrategyAC):
             # This is not particularly smart, not fast. Could I do better ?
             for (ind, w_val) in enumerate(w_vals):
                 G_mat[ind][x_ip1_ind[ind]-1:x_ip1_ind[ind]+1] = np.array([1-w_val, w_val])
+            # ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
 
             # Create a new dataframe to keep the interpolated stuff
             new_data = pd.DataFrame(new_tdt, columns=this_data.columns)
@@ -207,18 +143,16 @@ class ResampleGDPStrategy(MPStrategyAC):
                     # terms for the merged profile.
                     V_mat = G_mat @ U_mat @ G_mat.T
 
-                    # TODO: confirm what happens with NaNs.
-
-                    # Most likely, I will have som enon-diagonal elements tothis matrix, indicative
-                    # of correlations between the interpolated points (this will happen if a bin
-                    # edge is used for two interpolated points, which is not that exotic).
-                    # I can't really do anything with these for now, so I'll simply issue a
-                    # warning.
+                    # Most likely, I will have some non-diagonal elements to this matrix,
+                    # indicative of correlations between the interpolated points (this will happen
+                    # if a bin edge is used for two interpolated points, which is not that rare).
+                    # I can't really do anything with this information for now, so I'll simply
+                    # issue a warning.
                     # TODO: could one do better and account for this "somehow" ? This would most
                     # likely not be an easy update.
                     if not np.array_equal(V_mat[(V_mat != 0)|(np.isnan(V_mat))],
                                           V_mat.diagonal(), equal_nan=True):
-                        logger.warning("Correlation between interpolated profile elements " +
+                        logger.warning("Correlation between resampled profile elements " +
                                        " is being ignored. This is bad !")
 
                     # Assign the values to the new_data DataFrame
