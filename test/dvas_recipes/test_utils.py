8c2ff3a5ac2a6565c9fc50d03aa2641f85c7bea2
13974615+fpavogt <13974615+fpavogt@users.noreply.github.com>
2021-06-09 18:08:27 +0200

    Disable outdated test

--- test/dvas_recipes/test_utils.py
+++ test/dvas_recipes/test_utils.py
@@ -8,22 +8,3 @@ SPDX-License-Identifier: GPL-3.0-or-later
 Module contents: Testing classes and function for dvas_recipes.utils module.
 
 """
-
-from pathlib import Path
-
-# Import module to test
-from dvas_recipes import utils as dru
-
-def test_initialize_recipe():
-    """ Testing the recipe initialization routine. """
-
-    this_file_path = Path(__file__).resolve()
-
-    try:
-        # This will fail, because the recipe configuratiomn file does not exist.
-        # But I use the error to make sure I was looking for the correct file.
-        # Adapted from the reply of boertel and Mingye Wang on StackOverflow:
-        # https://stackoverflow.com/questions/40666924
-        dru.initialize_recipe(this_file_path)
-    except FileNotFoundError as not_found:
-        assert Path(not_found.filename) == this_file_path.with_suffix('.yml')
