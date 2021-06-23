45235862ccc53ff33ea229f1a562779b24cc80c9
13974615+fpavogt <13974615+fpavogt@users.noreply.github.com>
2021-06-23 15:50:42 +0200

    Make 'has_flg()' return 0 if flg is <NA>

--- src/dvas/data/strategy/data.py
+++ src/dvas/data/strategy/data.py
@@ -406,9 +406,8 @@ class Profile(ProfileAC):
 
         """
         bit_nbr = self._get_flg_bit_nbr(val)
-        # Return 1 if the flag is set, 0 if it isn't, and <NA> if the flag was not set.
-        return self.flg.apply(lambda x: (x >> bit_nbr) & 1 if not pd.isna(x) else x)
-
+        # Return 1 if the flag is set, 0 if it isn't, EVEN if the flag was not set (ie flg is <NA>).
+        return self.flg.apply(lambda x: (x >> bit_nbr) & 1 if not pd.isna(x) else 0)
 
 class RSProfile(Profile):
     """ Child Profile class for *basic radiosonde* atmospheric measurements.
