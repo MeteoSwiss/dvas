6412bc0241bdc97c4a1eb5de3acd02ae74900e59
13974615+fpavogt <13974615+fpavogt@users.noreply.github.com>
2022-02-24 08:12:37 +0100

    Update docstring

--- src/dvas_recipes/uaii2022/dtas.py
+++ src/dvas_recipes/uaii2022/dtas.py
@@ -5,7 +5,7 @@ Distributed under the terms of the GNU General Public License v3.0 or later.
 
 SPDX-License-Identifier: GPL-3.0-or-later
 
-Module content: high-level delta recipes for the UAII22 campaign
+Module content: high-level delta recipes for the UAII2022 campaign
 """
 
 # Import from python
