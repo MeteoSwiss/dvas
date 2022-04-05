90698cae259c32bc60e170d25ef087ea9d847c5d
13974615+fpavogt <13974615+fpavogt@users.noreply.github.com>
2022-04-05 16:38:08 +0200

    Adjust the handling of recipe tags

--- src/dvas_recipes/uaii2022/tools.py
+++ src/dvas_recipes/uaii2022/tools.py
@@ -9,25 +9,30 @@ Module content: UAII2022 recipe tools
 """
 
 
-def get_query_filter(tags_in: list = None, tags_out: list = None) -> str:
+def get_query_filter(tags_in: list = None, tags_out: list = None, mids: list = None) -> str:
     """ Assembles a str to query the dvas DB, given a list of tags to include and/or exclude.
 
     Args:
         tags_in (list, optional): list of tags required to be present
         tags_out (list, optional): list of tags required to be absent
+        mids (list, optional): list of mids required
 
     Returns:
         str: the query filter
     """
 
+    filt = []
+
     if tags_in is not None:
-        filt_in = "tags('" + "'), tags('".join(tags_in) + "')"
-    else:
-        filt_in = ''
+        filt += ["tags('" + "'), tags('".join(tags_in) + "')"]
 
     if tags_out is not None:
-        filt_out = "not_(tags('" + "')), not_(tags('".join(tags_out) + "'))"
-    else:
-        filt_out = ''
+        filt += ["not_(tags('" + "')), not_(tags('".join(tags_out) + "'))"]
 
-    return f'and_({filt_in}, {filt_out})'
+    if mids is not None:
+        filt += ["mid('" + "'), mid('".join(mids) + "')"]
+
+    if len(filt) == 0:
+        return ''
+
+    return 'and_(' + ', '.join(filt) + ')'
--- src/dvas_recipes/utils.py
+++ src/dvas_recipes/utils.py
@@ -10,6 +10,9 @@ This module contains general dvas_recipe utilities.
 
 from pathlib import Path
 
+# Import from this module
+from . import dynamic
+
 
 def demo_storage_path():
     """ Returns the absolute path to the stored dvas demo locations, from where the dvas demo
@@ -40,13 +43,14 @@ def default_arena_path():
     return Path('.', 'dvas_proc_arena')
 
 
-def fn_suffix(eid=None, rid=None, var=None, tags=None):
+def fn_suffix(eid=None, rid=None, var=None, mids=None, tags=None):
     """ Returns the default suffix of filenames given a set of info provided by the user.
 
     Args:
         eid (int, optional): the event id
         rid (int, optional): the rig id
         var (str, optional): the variable name
+        mids (lost of str, optional): the list of mids
         tags (list of str, optional): the list of tags associated with the data
 
     Returns:
@@ -58,7 +62,49 @@ def fn_suffix(eid=None, rid=None, var=None, tags=None):
         if item is not None:
             suffix += '_{}'.format(item.replace(':', ''))
 
+    if mids is not None:
+        suffix += '_{}'.format('-'.join(mids))
+
     if tags is not None:
         suffix += '_{}'.format('-'.join(tags))
 
     return suffix[1:] if len(suffix) > 0 else None
+
+
+def format_tags(tags):
+    """ Formats a list of tags according to some basic rules. Any tag set by the user should be
+    fed to this routine.
+
+    Args:
+        tags (str, list): the tags to format
+
+    Returns:
+        list: the cleaned-up tags
+    """
+
+    # First, be nice with str inputs = single tag provided.
+    if isinstance(tags, str):
+        tags = [tags]
+
+    return tags
+
+
+def rsid_tags(pop=None):
+    """ Returns the list of rsid tags, possibly by removing some of them.
+
+    Args:
+        pop (list, optional): if set, any tags in this list will be removed from the returned list.
+
+    Returns:
+        list: rsid tags, minus the popped ones.
+    """
+
+    tags_out = dynamic.ALL_STEP_IDS
+
+    # If warranted, remove some of the tags ...
+    if pop is not None and tags_out is not None:
+        if isinstance(pop, str):
+            pop = [pop]
+        tags_out = [item for item in tags_out if item not in pop]
+
+    return tags_out
