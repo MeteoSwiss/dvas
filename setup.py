66a6f550ae5283fe207555ae860661796efd7d11
13974615+fpavogt <13974615+fpavogt@users.noreply.github.com>
2023-01-11 12:10:38 +0100

    Minor doc update

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 
     include_package_data=True,  # So that non .py files make it onto pypi, and then back !
